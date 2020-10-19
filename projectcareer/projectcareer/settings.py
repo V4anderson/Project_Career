3ed1bdf420c69e0ecaffaec0d2890d066f8f94fe
V4anderson <vandersonandroid321@gmail.com>
2020-10-19 16:25:23 -0300

    Crud Simple

--- projectcareer/projectcareer/settings.py
+++ projectcareer/projectcareer/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'td$qf2a+w##u$hx)5k5o1+wm65@oew60*6zysi4y!*7#!%gtjs'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
